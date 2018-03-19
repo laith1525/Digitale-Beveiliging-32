745f988c1ab7dcff6dd3d764d0d519ea7b1c8087
laith1525 <34649577+laith1525@users.noreply.github.com>
2018-03-19 18:55:09 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,30 @@
+$(function() {
+	$("body").css({"background-color": "#558C89"});
+
+
+
+	
+	$("#container").css({"width": "100%"});
+
+
+
+
+	$(".top").css({"margin-left": "1,7%","margin-top": "1,7%","width": "100%"});
+	$(".ghost").css({"padding": "5px","width": "21%", "float": "left"});
+	$(".Logo").css({"background-color" : "#558C89", "margin-left" : "auto", "margin-right" : "auto", "width" : "79%", "font-size" : "52px", "text-align" : "center"});
+	$("input\[type=text\]").css({"margin-top" : "5px", "margin-left" : "20px", "padding" : "5px", "width" : "50%", "-webkit-transition" : "width 0.4s ease-in-out", "transition" : "width 0.4s ease-in-out" });
+	$("input\[type=text\]\:focus").css({"width" : "87%"});
+	$("input\.text\[type=text\]").css({"margin-top" : "5px", "margin-left" : "20px", "padding" : "5px", "width" : "200px", "-webkit-transition" : "width 0.4s ease-in-out", "transition" : "width 0.4s ease-in-out"});
+	$("input\.text\[type=text\]\:focus").css({});
+	$("a\:link, a\:visited").css({"text-decoration" : "none", "color" : "black", "padding" : "5px"});
+	$("a\.wikipedia").css({"text-decoration" : "underline"});
+	$(".main").css({"margin-left" : "30px", "margin-top" : "30px"});
+	$(".balk").css({"background-color" : "#D9853B", "width" : "21%", "padding" : "5px", "float" : "left", "border-radius" : "10px", "border" : "1px"});
+	$(".content").css({"background-color" : "#D9853B", "width" : "53%", "float" : "left", "margin-left" : "75px", "margin-right" : "75px", "font-size" : "18px", "padding" : "20px"});
+	$(".Title").css({"margin-top" : "10px", "height" : "40px", "border-bottom" : "2px dotted"});
+	$(".alinea").css({"margin-top" : "10px"});
+	$("li").css({"font-size" : "20px", "padding" : "2px"});
+	$(".footer").css({"margin-top" : "612px", "width" : "100%", "background-color" : "grey", "text-align" : "center", "margin-right" : "5px"});
+	$(".input").css({});
+	$(".img_antivirus").css({"width" : "100px", "height" : "100px"});
+});
